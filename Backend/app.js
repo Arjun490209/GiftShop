a6f3171111d16fc53d4a95ac093ca97ee8b8c353
Arjun8400 <youji099@gmail.com>
2025-12-03 00:37:03 +0530

    asd

--- Backend/app.js
+++ Backend/app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const apiRouter =require("./routes/api")
 const connectDB = require("./config/db") //! database ko bulaya
 connectDB()     //! database ko run kiye
+const serverless = require("serverless-http");
 
 
 
@@ -24,7 +25,11 @@ app.use("/api", apiRouter)
 app.get("/", (req, res)=>{
     res.send("API is running...")
 })
-let port = process.env.PORT || 5000
-app.listen(port, ()=>{
-    console.log(`Server Running port ${port}`)
-})
+// let port = process.env.PORT || 5000
+// app.listen(port, ()=>{
+//     console.log(`Server Running port ${port}`)
+// })
+
+
+module.exports = app;
+module.exports.handler = serverless(app);
